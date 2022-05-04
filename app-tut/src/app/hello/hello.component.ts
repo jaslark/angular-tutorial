601101bf36e21a50efad76dfe1a2d8acf3850855
Trung Le <lenguyen0502@gmail.com>
2022-05-05 01:22:23 +0700

    04-communication-subject

--- app-tut/src/app/hello/hello.component.ts
+++ app-tut/src/app/hello/hello.component.ts
@@ -40,22 +40,22 @@ export class HelloComponent
     this._dataService.setTextFromHello(this.text);
   }
   ngOnChanges(changes: SimpleChanges): void {
-    console.log('Child OnChanges ran', { changes });
+    // console.log('Child OnChanges ran', { changes });
   }
   ngOnDestroy(): void {
-    console.log('Child OnDestroy ran');
+    // console.log('Child OnDestroy ran');
   }
   ngAfterViewInit(): void {
-    console.log('Child AfterViewInit ran');
+    // console.log('Child AfterViewInit ran');
   }
   ngAfterContentInit(): void {
-    console.log('Child AfterContentInit ran');
+    // console.log('Child AfterContentInit ran');
   }
   ngAfterViewChecked(): void {
-    console.log('Child AfterViewChecked ran');
+    // console.log('Child AfterViewChecked ran');
   }
   ngAfterContentChecked(): void {
-    console.log('Child AfterContentChecked ran');
+    // console.log('Child AfterContentChecked ran');
   }
 
 
