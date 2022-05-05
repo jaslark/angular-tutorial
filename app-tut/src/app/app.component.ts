a0d9f59fd5647e204c2c32700177c1108ec993ad
Trung Le <lenguyen0502@gmail.com>
2022-05-05 21:30:59 +0700

    06-pipe

--- app-tut/src/app/app.component.ts
+++ app-tut/src/app/app.component.ts
@@ -1,10 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { finalize, Observable, startWith, takeWhile, timer } from 'rxjs';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-tut';
+  now = "2020-06-24T09:00:00.000Z";
+  userId = "4321245";
+  userIdChangeAfterFiveSeconds = "14324";
+
+  users: User[] = [
+    {
+      name: "AA",
+      age: 30
+    },
+    {
+      name: "BB",
+      age: 28
+    },
+    {
+      name: "CCC",
+      age: 29
+    },
+    {
+      name: "DDD",
+      age: 16
+    }
+  ];
+
+  newUser: User;
+
+  constructor() {
+    this.newUser = new User();
+  }
+
+  ngOnInit() {
+  }
+
+  addUser() {
+    this.users.push(this.newUser);
+    this.newUser = new User();
+  }
+
+  addUserByUpdateReference() {
+    this.users = [...this.users, this.newUser];
+    this.newUser = new User();
+  }
 }
--- app-tut/src/app/app.module.ts
+++ app-tut/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppTitlePipe } from './app-title.pipe';
+import { IsAdultPipe } from './is-adult.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppTitlePipe,
+    IsAdultPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
